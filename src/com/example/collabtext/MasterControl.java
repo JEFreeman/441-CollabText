6d76202e129079beac18156a88bdcfa4f67e5d4b
Jason Freeman <jefreem@umich.edu>
2013-09-09 21:53:24 -0400

    MasterControl file

---
+++ src/com/example/collabtext/MasterControl.java
@@ -0,0 +1,38 @@
+package com.example.collabtext;
+
+public class MasterControl {
+
+	public void insert(char[] text, int userID){
+		//use the user ID to find the local pointer location
+		//or, be able to find local user... setUser()?
+		
+	}
+	
+	public void remove(){
+		
+	}
+	
+	public void replace(){
+		
+	}
+	
+	public void move(){
+		
+	}
+	
+	public void cursorLoc(){
+		//contain new actual index
+		//contain relative change in index
+		
+	}
+	
+	public void undo(){
+		
+	}
+	
+	public void redo(){
+		
+	}
+	
+}
+
