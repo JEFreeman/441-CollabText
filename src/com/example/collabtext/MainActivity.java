25a1f392c111c21f8d5d5294a1a3df4a64c2d299
Jason Freeman <jefreem@umich.edu>
2013-09-13 01:59:21 -0400

    changed UI, added structure for insert text

--- src/com/example/collabtext/MainActivity.java
+++ src/com/example/collabtext/MainActivity.java
@@ -10,6 +10,9 @@ import android.view.View.OnClickListener;
 import android.widget.Button;
 
 public class MainActivity extends Activity {
+	
+	MasterControl lineCom = new MasterControl();
+	//long int userID //based on surface/tablet's serial#?
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,22 @@ public class MainActivity extends Activity {
 	public void create(View view){	
 		Intent i = new Intent(this, DocEdit.class);
 		startActivity(i);
+	}
+	
+	public void sendText(View view){
+		//handle incoming text from txtbox and forward it to the control
+		//this puts the onus of deciding how large of a chunk to send at any one given time on the user.
+		//will have to add a separate button for deleting. That way we dont have to stitch text into the box
+		
+		//view.getObjectbyID(sendTxt); 
+		//lineCom.insert(uniqUserID, sendTxt.text, timestamp?);
+		
+		
+	}
+	
+	public void movePointer(View view){
+		//update this user's pointer location, and shift focus automatically to the text box
+		
 	}
 	
 	@Override
--- src/com/example/collabtext/MasterControl.java
+++ src/com/example/collabtext/MasterControl.java
@@ -2,9 +2,15 @@ package com.example.collabtext;
 
 public class MasterControl {
 
-	public void insert(char[] text, int userID){
+	public void insert(int userID, char[] text, int timestamp){
 		//use the user ID to find the local pointer location
 		//or, be able to find local user... setUser()?
+		
+		//idea. move pointer by clicking, when selecting the stage, 
+		//type in a box. this allows the user to make large edits and broadcast them instead of piecemeal
+		//insert would effectively just be a broadcast of this box.
+		
+		//send text to the collabrify server.
 		
 	}
 	
